26921ba6b67433a9169ebc503cf6c39a31619c4b
abdel792 <abdelkrim.bensaid@gmail.com>
2023-10-20 19:30:02 +0200

    Fxied a syntax error in addon/globalPlugins/conjugaison/__init__.py

--- addon/globalPlugins/conjugaison/__init__.py
+++ addon/globalPlugins/conjugaison/__init__.py
@@ -138,17 +138,17 @@ class GlobalPlugin (globalPluginHandler.GlobalPlugin):
 		rsearch = re.compile(pattern4, re.S)
 		pattern5 = r"</?(?:span|sup|b)[^>]*>"
 		rsub = re.compile(pattern5, re.M)
-		group = rsub.sub("", rsearch.search(req).group(1)).strip()
 		temps = [x for x in lst if not x in modes and not any(y in x for y in ("<ul", "&nbsp;"))]
 		order = [0, 1, 3, 2, 4, 5, 7, 6, 8, 9, 11, 10, 12, 13, 15, 14, 16, 17, 18, 20, 19, 21, 22, 24, 23, 25, 26, 27, 29, 28, 30, 31, 32, 33, 34, 36, 35, 37, 38, 39, 41, 40, 42]
-		infinitif = r"<h1>Le verbe {self.verb} est un auxiliaire.</h1>" if "---" in group else f"<h1>{group}.</h1>"
 		try:
-			lst=[lst[x] for x in order]
-		except IndexError:
+			group = rsub.sub("", rsearch.search(req).group(1)).strip()
+			infinitif = r"<h1>Le verbe {self.verb} est un auxiliaire.</h1>" if "---" in group else f"<h1>{group}.</h1>"
+		except AttributeError:
 			title = _("Error")
 			msg = "<h1>{0}</h1>".format(title)
 			msg += _("Can't conjugate the verb {0}").format(self.verb)
 		if not msg:
+			lst = [lst[x] for x in order]
 			frm = []
 			frm.append(infinitif)
 			for item in lst:
